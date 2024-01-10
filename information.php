b179b73c716851619dc36c4a14ec36d09d49d876
Kenta <kd1326756@st.kobedenshi.ac.jp>
2024-01-10 13:40:29 +0900

    20240110/14

---
+++ information.php
@@ -0,0 +1,33 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>利用方法</title>
+    <link rel="stylesheet" href="css/account.css">
+</head>
+
+<body>
+    <a href="home.php">
+        <h1 class="header-logo">NewRCode</h1>
+    </a>
+    <h2>利用方法</h2>
+    <div id="info">
+        <h3 class="HtU">NewR-Codeサイト</h3>
+        <p class="HtU">使い方を記載<br>使い方を記載<br>使い方を記載、多言語化対応未完成。</p>
+        <h3 class="HtU">NewR-Code公式LINE</h3>
+        <p class="HtU">使い方を記載<br>使い方を記載<br>使い方を記載、多言語化対応未完成。</p>
+        <img class="HtU" src="images/HtUSampleIMG.jpg">
+        <p class="HtU">上記画像は挿入時サンプルです。</p>
+        <h3 class="HtU">お問い合わせ</h3>
+        <p class="HtU">アカウント削除や不明点のお問い合わせは<a href="">こちら</a></p>
+    </div>
+
+    <button onclick="location.href='home.php'">ホームに戻る</button>
+
+</body>
+
+</html>
